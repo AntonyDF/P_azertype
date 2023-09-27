a3fa8be74380cf3d203b19e0b51698f0a9a0de36
AntonyDF <146179483+AntonyDF@users.noreply.github.com>
2023-09-27 18:27:27 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+lancerJeu()
+
+/*let zoneSaisie = document.getElementById("inputEcriture")
+console.log(zoneSaisie)
+
+let buttonValidation = document.getElementById("btnValiderMot")
+console.log(buttonValidation)
+
+let motPropose = document.querySelector(".zoneProposition")
+console.log(motPropose)
+
+let zoneScore = document.querySelector(".zoneScore span")
+console.log(zoneScore)
+
+let radioButtons = document.querySelectorAll(".optionSource input")
+console.log(radioButtons)*/
+
